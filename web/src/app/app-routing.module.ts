8a33923b30f70b7ef579e0201194a51b8aed0771
Gonzalo de la Vega <gadelavega@gmail.com>
2021-02-12 17:38:04 -0300

    Inital commit

---
+++ web/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {InicioComponent} from './pages/inicio/inicio.component';
+
+
+const routes: Routes = [
+
+  { path: 'inicio', component: InicioComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'inicio'}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ web/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { NavbarComponent } from './component/shared/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
